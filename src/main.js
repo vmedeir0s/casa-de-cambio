cf32b44c3f1b4af2b7b616074cc8190cce74b1a9
vmedeir0s <vmeedeiros@gmail.com>
2023-04-24 00:39:25 -0300

    feat: add treatment error

--- src/main.js
+++ src/main.js
@@ -1,3 +1,4 @@
+import Swal from 'sweetalert2';
 import { renderCoins } from './components';
 import './style.css';
 
@@ -6,13 +7,33 @@ const coinsInputEl = document.querySelector('#coin-input');
 const coinContentEl = document.querySelector('.coin-content');
 
 async function fetchRates(searchCoin = 'BRL') {
-  const response = await fetch(`https://api.exchangerate.host/latest?base=${searchCoin}`);
-  const result = await response.json();
-  return result;
+  try {
+    const response = await fetch(`https://api.exchangerate.host/latest?base=${searchCoin}`);
+    const result = await response.json();
+    if (searchCoin.toUpperCase() !== result.base) {
+      throw new Error('Digite uma moeda válida');
+    }
+    return result;
+  } catch (error) {
+    Swal.fire({
+      icon: 'error',
+      title: 'Moeda Inválida',
+      text: error.message,
+    });
+    return null;
+  }
 }
 
 async function handleSearch() {
   const searchedCoin = coinsInputEl.value;
+  if (!searchedCoin) {
+    Swal.fire({
+      icon: 'error',
+      title: 'Erro Inesperado',
+      text: 'Digite uma moeda',
+    });
+    return;
+  }
   const result = await fetchRates(searchedCoin);
   const rates = await result.rates;
   const ratesArray = Object.entries(rates);
