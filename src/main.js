89eda7ea799d761d70ff7d3a66bcd99596c01fcb
Vinicius <vmeedeiros@gmail.com>
2024-11-20 15:43:15 -0300

    refact: fix

--- src/main.js
+++ src/main.js
@@ -6,13 +6,19 @@ const searchBtnEl = document.querySelector('.search-btn');
 const coinsInputEl = document.querySelector('#coin-input');
 const coinContentEl = document.querySelector('.coin-content');
 
-async function fetchRates(searchCoin = 'BRL') {
+async function fetchRates(searchCoin) {
   try {
-    const response = await fetch(`https://api.exchangerate.host/latest?base=${searchCoin}`);
+    const API_KEY = import.meta.env.VITE_API_KEY;
+    const response = await fetch(
+      `https://v6.exchangerate-api.com/v6/${API_KEY}/latest/${
+        searchCoin || 'BRL'
+      }`
+    );
     const result = await response.json();
-    if (searchCoin.toUpperCase() !== result.base) {
+    if (result.result == 'error') {
       throw new Error('Digite uma moeda válida');
     }
+    coinsInputEl.value = '';
     return result;
   } catch (error) {
     Swal.fire({
@@ -26,22 +32,17 @@ async function fetchRates(searchCoin = 'BRL') {
 
 async function handleSearch() {
   const searchedCoin = coinsInputEl.value;
-  if (!searchedCoin) {
-    Swal.fire({
-      icon: 'error',
-      title: 'Erro Inesperado',
-      text: 'Digite uma moeda',
-    });
-    return;
-  }
+
   const result = await fetchRates(searchedCoin);
-  const rates = await result.rates;
+  const rates = await result.conversion_rates;
   const ratesArray = Object.entries(rates);
 
-  const ratesArrayObject = ratesArray.map((rate) => (
-    { coinName: rate[0], value: rate[1].toFixed(2) }));
+  const ratesArrayObject = ratesArray.map((rate) => ({
+    coinName: rate[0],
+    value: rate[1].toFixed(2),
+  }));
   coinContentEl.style = 'overflow-y: auto';
-  renderCoins(ratesArrayObject, result.base);
+  renderCoins(ratesArrayObject, result.base_code);
 }
 
 searchBtnEl.addEventListener('click', handleSearch);
